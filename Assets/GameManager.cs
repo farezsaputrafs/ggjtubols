2aa03b0c99345a7f8189e92181498bbaa2e134d3
andreas.gagas <andreas.gagas@gmail.com>
2024-01-27 22:00:52 +0700

    asdfg

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -15,6 +16,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.F12))
+            SceneManager.LoadScene("SampleScene v2");
     }
 }
